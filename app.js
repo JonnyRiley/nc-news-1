b8a8bc8c4718957969ece9a448cccd5665b4201d
JonnyRiley <56361035+JonnyRiley@users.noreply.github.com>
2020-02-03 15:08:58 +0000

    405 Error for users

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const apiRouter = require("./routes/api_router");
 const { customErrors } = require("./errors");
 app.use(express.json());
-
 app.use("/api", apiRouter);
+app.all("/*", (request, response, next) => {
+  next({ status: 405, msg: "Route pathway NOT FOUND" });
+});
 app.use(customErrors);
 module.exports = app;
