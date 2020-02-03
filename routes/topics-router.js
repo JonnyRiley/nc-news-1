19daba9bfb17bc82f5f33e8a5306617769b0c871
JonnyRiley <56361035+JonnyRiley@users.noreply.github.com>
2020-02-03 11:25:40 +0000

    Upto Seeding

--- routes/topics-router.js
+++
@@ -1,9 +0,0 @@
-const topicsRouter = require("express").Router();
-const { sendTopics } = require("../controllers/topics-controller");
-const { send405Error } = require("../errors/index");
-
-topicsRouter
-  .route("/")
-  .get(sendTopics)
-  .all(send405Error);
-module.exports = topicsRouter;
